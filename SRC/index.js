fa60d3c0b71c7f14244a86e90cbbbc04e0694d6d
arianagaila <95535810+arianagaila@users.noreply.github.com>
2021-12-12 18:12:07 -0500

    Add Search Engine

--- SRC/index.js
+++ SRC/index.js
@@ -42,7 +42,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "abe8133e2a18c1d21a8707cbc98dbdb8";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "abe8133e2a18c1d21a8707cbc98dbdb8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
