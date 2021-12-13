3f712fee65f23c4bc572930fcc1074e01a5848b8
arianagaila <95535810+arianagaila@users.noreply.github.com>
2021-12-12 20:08:18 -0500

    Fix icon and date and time bugs

--- SRC/index.js
+++ SRC/index.js
@@ -32,9 +32,9 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  let dateElement = document.querySelector("date");
+  let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  let iconElement = document.querySelector("icon");
+  let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
