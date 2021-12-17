0c3cd9b6443d3e4e29130207cf8dc13b65032d30
arianagaila <95535810+arianagaila@users.noreply.github.com>
2021-12-17 13:55:53 -0500

    Added Forecast JS Loop (HTML)

--- SRC/index.js
+++ SRC/index.js
@@ -21,6 +21,27 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thur"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` <div class="col-2">
+        <div class="forecast-date">
+          ${day} </div>
+         <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="" width="42" />
+         <div class="forecast-temp"> 
+         <span class="forecast-temp-max">18°</span> 
+         <span class="forecast-temp-min">12°</span> 
+       </div>
+      </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -75,6 +96,8 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
+
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
