a978eaaa436f16496693db860f25eb8701c24458
arianagaila <95535810+arianagaila@users.noreply.github.com>
2021-12-19 11:39:47 -0500

    Working Basic Weather App

--- SRC/index.js
+++ SRC/index.js
@@ -59,7 +59,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "abe8133e2a18c1d21a8707cbc98dbdb8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
